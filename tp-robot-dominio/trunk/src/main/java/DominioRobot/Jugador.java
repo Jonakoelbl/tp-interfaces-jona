c0de30150427d83590fc46377bdbbe50d06910f6
jonakoelbl <jonakoelbl@7feefa9b-1541-1d92-7568-9b47635faafd>
2012-08-29 01:05:13 +0000

    Jugador:

--- tp-robot-dominio/trunk/src/main/java/DominioRobot/Jugador.java
+++ tp-robot-dominio/trunk/src/main/java/DominioRobot/Jugador.java
@@ -1,4 +1,43 @@
 package DominioRobot;
 
+import java.util.List;
+import java.util.Vector;
+
 public class Jugador {
+	
+	private String nombre; 
+	private List<Robot> misRobots = new Vector<Robot>();
+	private Integer dinero = 3000;
+	
+	public Jugador(String unNombre) {
+		this.nombre = unNombre;
+	}
+	
+	public void repararUnRobot(Robot unRobot){
+		Sistema.getInstancia().reparar(unRobot, this);
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public List<Robot> getMisRobots() {
+		return misRobots;
+	}
+
+	public void setMisRobots(List<Robot> misRobots) {
+		this.misRobots = misRobots;
+	}
+
+	public Integer getDinero() {
+		return dinero;
+	}
+
+	public void setDinero(Integer dinero) {
+		this.dinero = dinero;
+	}
 }
