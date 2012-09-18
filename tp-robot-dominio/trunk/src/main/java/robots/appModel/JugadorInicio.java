07af7dca89a1dafdc020b89a64c17451bfb557a4
jonakoelbl <jonakoelbl@7feefa9b-1541-1d92-7568-9b47635faafd>
2012-09-18 20:46:17 +0000

    Creacion de una aplicacion model de jugador (adapter entre el modelo y la interface )

---
+++ tp-robot-dominio/trunk/src/main/java/robots/appModel/JugadorInicio.java
@@ -0,0 +1,56 @@
+package robots.appModel;
+
+import java.util.List;
+
+import org.uqbar.commons.model.ObservableUtils;
+import org.uqbar.commons.utils.Observable;
+
+import DominioRobot.Jugador;
+import DominioRobot.Robot;
+
+@Observable
+public class JugadorInicio {
+	
+	public static final String ROBOT_SELECCIONADO = "robotSeleccionado";
+	public static final String ROBOTS_DEL_JUGADOR = "robots";
+	private Jugador jugador;
+	private Robot robotSeleccionado;
+	
+	public JugadorInicio(Jugador jugador) {
+		super();
+		this.jugador = jugador;
+	}
+	
+	public Jugador getJugador() {
+		return jugador;
+	}
+	public void setJugador(Jugador jugador) {
+		this.jugador = jugador;
+	}
+	public Robot getRobotSeleccionado() {
+		return robotSeleccionado;
+	}
+	public void setRobotSeleccionado(Robot robotSeleccionado) {
+		this.robotSeleccionado = robotSeleccionado;
+	}
+	
+	public List<Robot> getRobots(){
+		return this.jugador.getMisRobots();
+	}
+	
+	public void setRobots(List<Robot> robots){
+		this.jugador.setMisRobots(robots);
+		ObservableUtils.forceFirePropertyChanged(this,"robots",this.getRobots());
+	}
+	
+	
+	public void vender(){
+		this.getJugador().vender(this.getRobotSeleccionado());
+		List<Robot> robots = this.getRobots();
+		this.setRobots(null);
+		this.setRobots(robots);
+	}
+	
+	
+
+}
