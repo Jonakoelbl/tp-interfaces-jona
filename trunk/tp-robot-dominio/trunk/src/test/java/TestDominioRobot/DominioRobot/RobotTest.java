f53deac83a8737a3ad9881c1a4c61e9fba37a8b5
diego.a.turchak@gmail.com <diego.a.turchak@gmail.com@7feefa9b-1541-1d92-7568-9b47635faafd>
2012-09-23 07:12:07 +0000

    Test unitarios clases...revisar jugador y tienda se encontro...

---
+++ trunk/tp-robot-dominio/trunk/src/test/java/TestDominioRobot/DominioRobot/RobotTest.java
@@ -0,0 +1,68 @@
+package TestDominioRobot.DominioRobot;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import DominioRobot.Jugador;
+import DominioRobot.Mejora;
+import DominioRobot.Robot;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RobotTest {
+	Mejora unaMejora;
+	Robot unRobot;
+	Jugador unUsuario;
+	Integer puntosReparacion;
+	@Before
+	public void setUp() throws Exception {
+		this.unRobot = new Robot("Diego");
+		this.puntosReparacion = 3;
+		this.unRobot.setNivelDeDeterioro(5);
+		this.unaMejora = mock(Mejora.class);
+		when(this.unaMejora.getMejoraDePoder()).thenReturn(3);
+		this.unUsuario = mock(Jugador.class);
+
+		
+	}
+/*	
+	protected void setUp() throws Exception {
+		this.cfp = mock(CallForPaper.class);
+		this.afincomp = new AfinidadComparator(this.cfp);
+	}*/
+
+	@Test
+	public void testRobot() {
+		assertEquals(this.unRobot.getNombreRobot(),"Diego");
+		Integer precioDeseado = this.unRobot.getPoderEfectivo()*50;
+		assertEquals(this.unRobot.getPrecio(),precioDeseado);
+	}
+
+	@Test
+	public void testActualizarPoder() {
+		Integer poder = this.unRobot.getPoder() + this.unaMejora.getMejoraDePoder();
+		this.unRobot.actualizarPoder(this.unaMejora);
+		assertEquals(this.unRobot.getPoder(),poder);
+	}
+
+	@Test
+	public void testFuisteReparado() {
+		Integer nivelDeterioroDeseado = this.unRobot.getNivelDeDeterioro() - this.puntosReparacion;;
+		this.unRobot.fuisteReparado(this.puntosReparacion);
+		assertEquals(this.unRobot.getNivelDeDeterioro(),nivelDeterioroDeseado);
+	}
+
+	@Test
+	public void testFuisteComprado() {
+		this.unRobot.fuisteComprado(this.unUsuario);
+		assertEquals(this.unRobot.getPropietario(),this.unUsuario);
+	}
+
+	@Test
+	public void testFuisteVendido() {
+		this.unRobot.fuisteComprado(this.unUsuario);
+		this.unRobot.fuisteVendido();
+		assertEquals(this.unRobot.getPropietario(),null);
+	}
+
+}
